2a4fbdd670c7a3e9a9f7a354cf6de598e1f4c46c
Raphael de Faria Rosa <99773236+RaphaelRosa74@users.noreply.github.com>
2025-04-11 16:19:29 -0300

    adicionado rotas ao button excluir e editar

--- costs-project/src/components/pages/Projects.jsx
+++ costs-project/src/components/pages/Projects.jsx
@@ -2,16 +2,19 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import Messages from "../layout/Messages";
-import Container from '../layout/Container'
-import LinkButton from '../layout/LinkButton'
+import Container from '../layout/Container';
+import LinkButton from '../layout/LinkButton';
+import Loading from '../layout/Loading'
 import ProjectCard from '../project/ProjectCard';
 
-import styles from './Projects.module.css'
+import styles from './Projects.module.css';
 
 function Projects() {
   const location = useLocation();
   const [message, setMessage] = useState(null);
   const [projects, setProjects] = useState([])
+  const [removeLoading, setRemoveLoading ] = useState(false)
+  const [projectMessage, setProjectMessage] = useState('')
 
   useEffect(() => {
     // Prioridade para o state do router (mais confiável)
@@ -32,6 +35,7 @@ function Projects() {
   }, [location.state]); // Executa quando location.state muda
 
   useEffect(() =>{
+    setTimeout(() => {
     fetch('http://localhost:5000/projects',{
       method: 'GET',
       headers:{
@@ -41,9 +45,26 @@ function Projects() {
       .then((resp) => resp.json())
       .then((data) => {
         setProjects(data)
+        setRemoveLoading(true)
       })
       .catch((err) => console.log(err))
+    }, 300)
   },[])
+
+  function removeProject(id){
+
+    fetch(`http://localhost:5000/projects/${id}`,{
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }).then(resp => resp.json)
+    .then(() => {
+      setProjects(projects.filter((project) => project.id !== id))
+      setProjectMessage('Projeto removido com sucesso!')
+    })
+    .catch(err => console.log(err))
+  }
 
   return (
     <div className={styles.project_container}>
@@ -51,7 +72,8 @@ function Projects() {
           <h1>Meus Projetos</h1>
           <LinkButton to="/newproject" text="Criar Projeto"/>
       </div>
-      {message && <Messages type={message.type} msg={message.text} />}
+      {message && <Messages type='success' msg={message.text} />}
+      {projectMessage && <Messages type='success' msg={projectMessage} />}
       <Container customClass="start">
       {projects.length > 0 &&
       projects.map((project) => (
@@ -61,8 +83,13 @@ function Projects() {
         budget={project.budget}
         category={project.category.name}
         key={project.id}
+        handleRemove={removeProject}
         />
       ))}
+      {!removeLoading && <Loading/> }
+      {removeLoading && projects.length === 0 && (
+        <p>Não há projetos cadastrados!</p>
+      )}
       </Container>
     </div>
   );
--- costs-project/src/routes/index.jsx
+++ costs-project/src/routes/index.jsx
@@ -4,6 +4,7 @@ import Company from "../components/pages/Company";
 import Projects from "../components/pages/Projects";
 import Contact from "../components/pages/Contact";
 import NewProject from "../components/pages/NewPoject";
+import Project from "../components/pages/Project";
 
 function RoutesContainer() {
   return (
@@ -13,6 +14,8 @@ function RoutesContainer() {
       <Route path="/company" element={<Company />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/newproject" element={<NewProject />} />
+      <Route path="/project/:id" element={<Project />} />
+
     </Routes>
   );
 }
