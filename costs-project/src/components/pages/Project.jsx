d57a58afe49cbee115e038ed58c2990755928f7a
Raphael de Faria Rosa <99773236+RaphaelRosa74@users.noreply.github.com>
2025-04-15 16:18:25 -0300

    ,

--- costs-project/src/components/pages/Project.jsx
+++ costs-project/src/components/pages/Project.jsx
@@ -1,26 +1,92 @@
+import styles from './Project.module.css'
+
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
+
+import Loading from '../layout/Loading'
+import Container from '../layout/Container'
+import Messages from '../layout/Messages'
+
+import ProjectForm from '../project/ProjectForm'
 
 function Project(){
     const {id} = useParams()
     const [project, setProject] = useState([])
+    const [showProjectForm, setShowProjectForm] = useState(false)
+    const[message, setMessage] = useState()
+    const[type, setType] = useState()
 
     useEffect(() =>{
-        fetch(`http://localhost:5000/projects/${id}`,{
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application.json',
+        setTimeout(() => {
+            fetch(`http://localhost:5000/projects/${id}`,{
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application.json',
+                },
+            })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setProject(data)
+            })
+            .catch((err) => console.log)
+        }, 300)
+    },[id])
+
+    function editPost(project){
+        //budget validation
+        if (project.budget < project.cost){
+            setMessage('O orçamento não pode ser menor que o custo do projeto')
+            setType('error')
+            return false
+        }
+        fetch(`http://localhost:5000/projects/${project.id}`,{
+            method: 'PATCH',
+            headers: {'Content-Type': 'application.json',
             },
+            body: JSON.stringify(project),
         })
         .then((resp) => resp.json())
         .then((data) => {
             setProject(data)
+            setShowProjectForm(false)
+            setMessage('Projeto atualizado com sucesso')
+            setType('success')
         })
-        .catch((err) => console.log)
-    },[id])
+        .catch((err) => console.log(err))
+    }
+    function toggleProjectForm(){
+        setShowProjectForm(!showProjectForm)
+    }
 
     return(
-        <p>{project.name}</p>
+       <>
+       {project.name ? (
+        <div className={styles.project_details}>
+            <Container customClass='column'>
+                {message && <Messages type={type} msg={message} />}
+                <div className={styles.details_container}>
+                    <h1>Projeto: {project.name}</h1>
+                    <button className={styles.btn} onClick={toggleProjectForm}>
+                        {!showProjectForm ? 'Editar projeto' : 'Fechar projeto'}
+                        </button>
+                        {!showProjectForm ? (
+                            <div className={styles.project_info}>
+                                <p><span>Categoria: </span> {project.category.name}</p>
+                                <p><span>Orçamento do Projeto: </span> R${project.budget}</p>
+                                <p><span>Total Utilizado: </span> R${project.cost}</p>
+                            </div>
+                        ) : (
+                            <div className={styles.project_info}>
+                                <ProjectForm handleSubmit={editPost} btnText='Concluir edição' projectData={project} />
+                            </div>
+                        )}
+                </div>
+            </Container>
+        </div>
+       ) : (
+       <Loading />
+       )}
+       </>
     )
 }
 
